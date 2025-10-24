b90bd452e81786a17744f679f671f72ebbf00f22
DevAyu-Codes <ayushkumar274549@gmail.com>
2025-10-25 02:37:34 +0530

    final test

---
+++ pages/js/cart.js
@@ -0,0 +1,7 @@
+// const userCart = [
+//                 { productID: 'dress001', size: 'M', quantity: 1 },
+//                 { productID: 'top002', size: 'S', quantity: 2 },
+//             ];
+//             if (!localStorage.getItem('userCart')) {
+//                 localStorage.setItem('userCart', JSON.stringify(demoCart));
+//             }
